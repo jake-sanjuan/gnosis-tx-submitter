794c6c1541988710441a6199546d80eb65073ea4
Jacob San Juan <contactjsj42@gmail.com>
2022-02-14 16:43:42 -0700

    Updated test for batching so it is in passing state

--- test/test-batch-transaction.ts
+++ test/test-batch-transaction.ts
@@ -7,7 +7,7 @@ import {sendBatchTransaction} from "../src/index";
 async function testBatchTransaction() {
     await sendBatchTransaction(
         "0x52967DA31f243Eb9F35f151643Ab9D30e445B1C6",
-        ["0x9e0bcE7ec474B481492610eB9dd5D69EB03718D5"],
+        ["0x9e0bcE7ec474B481492610eB9dd5D69EB03718D5", "0x9e0bcE7ec474B481492610eB9dd5D69EB03718D5"],
         ["0", "0"],
         ["0xabcd", "0xbced"],
         5
