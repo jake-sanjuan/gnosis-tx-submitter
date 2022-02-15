228cb91122317c9895a92ef094d03b27a5705ad9
Jacob San Juan <contactjsj42@gmail.com>
2022-02-14 18:25:09 -0700

    Combined and broke out functionality

--- src/index.ts
+++ src/index.ts
@@ -22,92 +22,31 @@ const TX_URL = "https://safe-transaction.goerli.gnosis.io";
 
 export async function sendTransaction(
     safeAddress: string,
-    transactionTargetAddress: string,
-    transactionValue: string,
-    transactionData: string,
-    chainId: number = 1, // Default mainnet 
-): Promise<void> {
-
-    const safeService = new SafeServiceClient(TX_URL);
-    const signer = getSigner(chainId)
-    const transaction: SafeTransactionDataPartial = {
-        to: transactionTargetAddress,
-        value: transactionValue,
-        data: transactionData,
-        nonce: await safeService.getNextNonce(safeAddress)
-    }
-    const adapter = new EthersAdapter({
-        ethers: ethers, 
-        signer: signer
-    });
-    const safe: Safe = await Safe.create({
-        ethAdapter: adapter,
-        safeAddress: safeAddress
-    });
-    const tx = await safe.createTransaction(transaction);
-    await safe.signTransaction(tx);
-    const txHash = await safe.getTransactionHash(tx);
-
-    safeService.proposeTransaction({
-        safeAddress: safeAddress,
-        safeTransaction: tx,
-        safeTxHash: txHash,
-        senderAddress: await signer.getAddress()
-    });
-}
-
-export async function sendBatchTransaction(
-    safeAddress: string,
     transactionTargetAddresses: string[],
     transactionValues: string[],
     transactionData: string[],
     chainId: number = 1
 ): Promise<void> {
 
+    let arrLength: number = transactionTargetAddresses.length;
     if(
-        transactionTargetAddresses.length != transactionData.length || 
-        transactionTargetAddresses.length != transactionValues.length
-    ) {
-        throw new Error("Mismatch array length");
+        arrLength !== transactionData.length || 
+        arrLength !== transactionValues.length
+    ) { 
+        throw new Error("Mismatch array length"); 
     }
 
-    const safeService  = new SafeServiceClient(TX_URL);
-    const signer = getSigner(chainId);
-    
-    const transactions: MetaTransactionData[] = [];
-    for (let i = 0; i < transactionTargetAddresses.length; i++) {
-        transactions.push({
-            to: transactionTargetAddresses[i],
-            value: transactionValues[i],
-            data: transactionData[i],
-        } as MetaTransactionData);
-    }
-    const adapter:EthersAdapter = new EthersAdapter({
-        ethers: ethers,
-        signer: signer
-    });
-    const safe:Safe = await Safe.create({
-        ethAdapter: adapter,
-        safeAddress: safeAddress
-    });
-
-    const options: SafeTransactionOptionalProps = {
-        nonce: await safeService.getNextNonce(safeAddress)
-    }
-
-    const tx = await safe.createTransaction([...transactions], options);
-    await safe.signTransaction(tx);
-    const txHash = await safe.getTransactionHash(tx);
-
-    console.log(transactions)
-    console.log(tx)
-
-    await safeService.proposeTransaction({
-        safeAddress:safeAddress,
-        safeTransaction: tx,
-        safeTxHash: txHash,
-        senderAddress: await signer.getAddress(),
-    });
+    const transactionArr = createTxArray(
+        arrLength,
+        transactionTargetAddresses,
+        transactionValues,
+        transactionData
+    );
+    await createAndSendSafeTx(
+        chainId,
+        safeAddress,
+        transactionArr
+    );
 }
 
 const getSigner = (chainId: number): Signer => {
@@ -117,4 +56,65 @@ const getSigner = (chainId: number): Signer => {
 
 const getProvider = (chainId: number): ethers.providers.AlchemyProvider =>  {
     return new ethers.providers.AlchemyProvider(chainId, process.env.ALCHEMY_KEY)
+}
+
+const createTxArray = (
+    arrLength: number,
+    addresses: string[],
+    values: string[],
+    data: string[]
+): SafeTransactionDataPartial[] | MetaTransactionData[] => {
+    let transactionArr: SafeTransactionDataPartial[] | MetaTransactionData[] = [];
+    if(arrLength === 1) {
+        transactionArr.push({
+            to: addresses[0],
+            value: values[0],
+            data: data[0]
+        } as SafeTransactionDataPartial);
+    } else {
+        for (let i = 0; i < arrLength; i++) {
+            transactionArr.push({
+                to: addresses[i],
+                value: values[i],
+                data: data[i],
+            } as MetaTransactionData);
+        }
+    }
+    return transactionArr;
+}
+
+// Call to other function to figure out URL
+const createAndSendSafeTx = async (
+    chainId: number,
+    safeAddress: string,
+    transactionArr: SafeTransactionDataPartial[] | MetaTransactionData[],
+): Promise<void> => {
+    const signer = getSigner(chainId);
+    const adapter: EthersAdapter = createAdapter(signer);
+    const safe:Safe = await Safe.create({
+        ethAdapter: adapter,
+        safeAddress: safeAddress
+    });
+
+    const safeService  = new SafeServiceClient(TX_URL);
+    const options: SafeTransactionOptionalProps = {
+        nonce: await safeService.getNextNonce(safeAddress)
+    }
+
+    const tx = await safe.createTransaction([...transactionArr], options);
+    await safe.signTransaction(tx);
+    const txHash = await safe.getTransactionHash(tx);
+    await safeService.proposeTransaction({
+        safeAddress:safeAddress,
+        safeTransaction: tx,
+        safeTxHash: txHash,
+        senderAddress: await signer.getAddress(),
+    });
+}
+
+const createAdapter = (signer: Signer): EthersAdapter => {
+    return new EthersAdapter({
+        ethers: ethers,
+        signer: signer
+    });
 }
--- test/test-batch-transaction.ts
+++ test/test-batch-transaction.ts
@@ -1,11 +1,11 @@
-import {sendBatchTransaction} from "../src/index";
+import {sendTransaction} from "../src/index";
 
 /**
  * Has to be validated on Gnosis UI
  */
 
 async function testBatchTransaction() {
-    await sendBatchTransaction(
+    await sendTransaction(
         "0x52967DA31f243Eb9F35f151643Ab9D30e445B1C6",
         ["0x9e0bcE7ec474B481492610eB9dd5D69EB03718D5", "0x9e0bcE7ec474B481492610eB9dd5D69EB03718D5"],
         ["0", "0"],
