c28fb779dba7f2bb929ec5c26d71195cda376820
Jacob San Juan <contactjsj42@gmail.com>
2022-03-09 08:35:52 -0700

    Removed dist from tracking

--- dist/index.d.ts
+++
@@ -1,1 +0,0 @@
-export declare function sendTransaction(safeAddress: string, transactionTargetAddresses: string[], transactionValues: string[], transactionData: string[], chainId?: number): Promise<void>;
